26e7d6596842930372766cc503275709dd5d9ea1
unknown <chiranjibpaul007@gmail.com>
2020-02-25 18:51:43 +0530

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,60 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        DigiCoKolkata
-      </h1>
-      <h2 class="subtitle">
-        My groovy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="container py-5">
+      <div class="d-flex justify-content-center flex-column">
+        <h2 class="text-center">ABOUT US</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        </p>
+      </div>
+    </div>
+    <div style="background-color:#fafafa">
+      <div class="container py-5">
+        <h2 class="text-center">STAR PRODUCTS</h2>
+        <div class="pt-3 row">
+          <div class="col-12 col-lg-6">
+            <img class="w-100" style src="/images/powerbank.png" alt />
+          </div>
+          <div class="col-12 col-lg-6">
+            <div class="d-flex">
+              <img class="w-100" src="/images/earphonebasic.png" alt />
+              <img class="w-100" src="/images/luggage.png" alt />
+            </div>
+            <div>
+              <img class="w-100" src="/images/smartband.png" alt />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-2" style="background-color:#fafafa">
+      <div class="container py-5">
+        <h2 class="text-center">HOT ACCESSORIES</h2>
+        <div class="mt-3 d-flex justify-content-center">
+          <span class="deals">Hot Deals</span>
+          <span class="deals ml-4">BestSeller</span>
+        </div>
+        <div class="row mt-3">
+          <div class="col-12 mt-2 mt-sm-0 col-lg-3">
+            <img src="/images/" alt />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.deals {
+  font-size: 30px;
+  color: #ff7a21;
+  opacity: 0.77;
+  border-bottom: 2px solid #fafafa;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.deals:hover {
+  opacity: 1;
+  border-bottom: 2px solid #ff7a21;
+  transition: 0.5s linear ease-in-out;
 }
 </style>
